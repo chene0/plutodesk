609961a2aca2e5406d1ab8a2fa9fb0f6e659e164
Ethan Chen <ethansunchen@gmail.com>
2025-08-18 21:25:52 -0400

    Install tauri-plugin-sql

--- frontend/src-tauri/src/lib.rs
+++ frontend/src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ use screenshot::{close_screenshot_overlay, take_screenshot};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![close_screenshot_overlay])
         .setup(|app| {
